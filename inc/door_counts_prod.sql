b3701311b64806268a7ec038ed80ca94a85dc413
Graham Hukill <Graham Hukill>
2017-08-23 11:19:50 -0400

    finalizing sql

--- inc/door_counts_prod.sql
+++ inc/door_counts_prod.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `door_stats`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `door_stats` (
-  `id` int(45) NOT NULL,
+  `id` int(45) NOT NULL AUTO_INCREMENT,
   `gate_number` int(45) NOT NULL,
   `location` varchar(255) NOT NULL,
   `building` varchar(255) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE `door_stats` (
   `original_timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `ip` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,74 @@ LOCK TABLES `door_stats` WRITE;
 /*!40000 ALTER TABLE `door_stats` DISABLE KEYS */;
 /*!40000 ALTER TABLE `door_stats` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER building_from_new_location_prod BEFORE INSERT
+ON data_counts.door_stats
+FOR EACH ROW 
+BEGIN
+
+DECLARE inipos INTEGER;
+DECLARE endpos INTEGER;
+DECLARE loc VARCHAR(100);
+DECLARE delim VARCHAR(1);
+    DECLARE fullstr VARCHAR(100);
+
+SET delim = '|';
+SET inipos = 1;
+SET fullstr = NEW.location;   
+    
+    
+SET endpos = LOCATE(delim, fullstr, inipos);
+SET loc = SUBSTR(fullstr, inipos, endpos - inipos);
+SET NEW.building = loc;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER building_from_updated_location_prod BEFORE UPDATE
+ON data_counts.door_stats
+FOR EACH ROW 
+BEGIN
+
+DECLARE inipos INTEGER;
+DECLARE endpos INTEGER;
+DECLARE loc VARCHAR(100);
+DECLARE delim VARCHAR(1);
+    DECLARE fullstr VARCHAR(100);
+
+SET delim = '|';
+SET inipos = 1;
+SET fullstr = NEW.location;
+SET endpos = LOCATE(delim, fullstr, inipos);
+SET loc = SUBSTR(fullstr, inipos, endpos - inipos);
+SET NEW.building = loc;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-23 11:00:29
+-- Dump completed on 2017-08-23 11:18:13
